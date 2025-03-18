c0cccaf6b16bca344bea00dc7613271461241980
dmitrijs <petja314@gmail.com>
2025-03-18 20:47:36 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { useEffect } from "react";
+const tg = window.Telegram.WebApp;
+
+function App() {
+  useEffect(() => {
+    tg.ready();
+  }, []);
+  const onClose = () => {
+    tg.close();
+  };
+  return (
+    <div className="App">
+      <h1>hello world!</h1>
+      <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
